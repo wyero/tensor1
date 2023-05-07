6026f4a5e5c6192aa897f9dd5671743436f7e9d0
Wyero JR <kajuwyero@gmail.com>
2023-05-07 10:31:17 +0700

    tensor1

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def predict():
     return render_template('index.html', prediction_text = '{}'.format(prediction))
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
